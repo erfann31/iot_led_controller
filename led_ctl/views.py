4dcd2d262ab91a1c0a44f5872d717b61cf35bddf
erfann31 <erfannasri2@gmail.com>
2024-06-21 12:15:15 +0330

    fix views

--- led_ctl/views.py
+++ led_ctl/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 import json
+
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Global variables to hold LED status and scheduling information
 led_status = "OFF"
@@ -11,12 +12,14 @@ def index(request):
     return render(request, 'index.html')
 
 def on_view(request):
-    global led_status
+    global led_status, scheduling
+    scheduling = {"time_on_ms": 0, "time_off_ms": 0}
     led_status = "ON"
     return JsonResponse({"status": led_status})
 
 def off_view(request):
-    global led_status
+    global led_status, scheduling
+    scheduling = {"time_on_ms": 0, "time_off_ms": 0}
     led_status = "OFF"
     return JsonResponse({"status": led_status})
 
@@ -31,7 +34,4 @@ def schedule_view(request):
         return JsonResponse({"status": "Scheduled", "scheduling": scheduling})
 
 def ping_view(request):
-    if led_status == "Scheduled":
-        return JsonResponse({"status": led_status, "scheduling": scheduling})
-    else:
-        return JsonResponse({"status": led_status})
+    return JsonResponse({"status": led_status, "scheduling": scheduling})
