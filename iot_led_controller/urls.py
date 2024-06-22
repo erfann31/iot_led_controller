6ad5c418abc4c2c4a654c33eabce5c63d98a063d
erfann31 <erfannasri2@gmail.com>
2024-06-22 06:22:53 +0330

    add Static Root

--- iot_led_controller/urls.py
+++ iot_led_controller/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,5 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index_view'),
     path('api/v1/led/', include('led_ctl.urls')),
-
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
