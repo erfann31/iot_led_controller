9c12af247ec8cfa73c1628d21f83bcdabd99d53a
erfann31 <erfannasri2@gmail.com>
2024-06-22 06:15:17 +0330

    fix urls

--- led_ctl/urls.py
+++ led_ctl/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import on_view, off_view, ping_view, schedule_view
 
 urlpatterns = [
-    path('on', on_view, name='on'),
-    path('off', off_view, name='off'),
+    path('on/', on_view, name='on'),
+    path('off/', off_view, name='off'),
     path('schedule', schedule_view, name='schedule'),
-    path('ping', ping_view, name='ping'),
+    path('ping/', ping_view, name='ping'),
 ]
--- led_ctl/views.py
+++ led_ctl/views.py
@@ -3,27 +3,35 @@ import json
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 
 # Global variables to hold LED status and scheduling information
 led_status = "OFF"
 scheduling = {"time_on_ms": 0, "time_off_ms": 0}
 
+
+@api_view(['GET'])
 def index(request):
     return render(request, 'index.html')
 
+
+@api_view(['GET'])
 def on_view(request):
     global led_status, scheduling
     scheduling = {"time_on_ms": 0, "time_off_ms": 0}
     led_status = "ON"
     return JsonResponse({"status": led_status})
 
+
+@api_view(['GET'])
 def off_view(request):
     global led_status, scheduling
     scheduling = {"time_on_ms": 0, "time_off_ms": 0}
     led_status = "OFF"
     return JsonResponse({"status": led_status})
 
-@csrf_exempt
+
+@api_view(['POST'])
 def schedule_view(request):
     global scheduling, led_status
     if request.method == 'POST':
@@ -33,5 +41,7 @@ def schedule_view(request):
         led_status = "Scheduled"
         return JsonResponse({"status": "Scheduled", "scheduling": scheduling})
 
+
+@api_view(['GET'])
 def ping_view(request):
     return JsonResponse({"status": led_status, "scheduling": scheduling})
